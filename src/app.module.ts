d5547bb12445e7b557587907f4c6eb05df1a2449
luismartinezkyb <18030545@itcelaya.edu.mx>
2024-03-14 20:50:25 -0600

    (feat): Implementación de middleware routes middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,17 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { NotFoundInterceptor } from './interceptors/not-found.interceptor';
-import { NotFoundMiddleware } from './interceptors/middleware-not-found';
+// import { APP_INTERCEPTOR } from '@nestjs/core';
+// import { NotFoundInterceptor } from './interceptors/not-found.interceptor';
+// import { NotFoundMiddleware } from './interceptors/middleware-not-found';
 
 @Module({
   imports: [UsersModule],
 
   providers: [
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: NotFoundInterceptor,
-    },
+    // {
+    //   provide: APP_INTERCEPTOR,
+    //   useClass: NotFoundInterceptor,
+    // },
   ],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(NotFoundMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,8 @@
-import { Logger, Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { ErrorHandlerService } from './error-handler.service';
+import { NotFoundMiddleware } from './interceptors/middleware-not-found';
 // import { APP_INTERCEPTOR } from '@nestjs/core';
 // import { NotFoundInterceptor } from '../interceptors/not-found.interceptor';
 
@@ -17,4 +18,8 @@ import { ErrorHandlerService } from './error-handler.service';
     ErrorHandlerService,
   ],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(NotFoundMiddleware).forRoutes('users/*');
+  }
+}
